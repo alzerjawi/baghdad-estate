4d2059997e321eec44d5eec470aad9329badcc8b
hassan <halzerjawi21@gmail.com>
2024-05-23 18:27:40 +0300

    Created template pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import SingUp from './pages/SingUp';
+import About from './pages/About'
+import Profile from './pages/Profile'
+import SignIn from './pages/Signin';
 
 export default function App() {
   return (
-    <div className='text-red-500'>no way</div>
+    <BrowserRouter >
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sing-in" element={<SignIn />} />
+        <Route path="/sing-out" element={<SingUp/>} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   )
 }

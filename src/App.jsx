56b0ac0cfe3828bc3e561ba34f7442876d3f4f53
hassan <halzerjawi21@gmail.com>
2024-05-23 19:31:27 +0300

    Header and linkage

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,17 @@ import Home from './pages/Home'
 import SingUp from './pages/SingUp';
 import About from './pages/About'
 import Profile from './pages/Profile'
-import SignIn from './pages/Signin';
+import SignIn from './pages/SignIn';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <BrowserRouter >
+    <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/sing-in" element={<SignIn />} />
-        <Route path="/sing-out" element={<SingUp/>} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-out" element={<SingUp/>} />
         <Route path="/about" element={<About />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
